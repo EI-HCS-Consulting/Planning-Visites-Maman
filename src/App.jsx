525f459d7769efa66b19a23000b8979633ada8ee
EI-HCS-Consulting <guillaume.frey38@gmail.com>
2026-05-14 21:20:04 +0200

    Add additional information to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ const RULES = [
   { icon: "🕐", text: "Créneaux : 11h, 13h, 15h, 17h" },
   { icon: "⏳", text: "Au moins 2h entre chaque visite" },
   { icon: "🌙", text: "1 personne peut rester la nuit" },
+  
   ];
 
 const C = {
